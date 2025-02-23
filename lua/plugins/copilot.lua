f3a95321ff16388ab22debf8a7d8b26f5e666391
vanm30 <vanikmatej99@gmail.com>
2025-02-23 17:39:07 +0000

    copilot

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,9 +1,9 @@
 return {
   -- Enable Copilot
   "zbirenbaum/copilot.lua",
-  cmd = "Copilot", -- This ensures Copilot is loaded when the command is called
+  cmd = "Copilot",         -- This ensures Copilot is loaded when the command is called
   build = ":Copilot auth", -- Ensures authentication for Copilot
-  event = "BufReadPost", -- You can also use `VimEnter` or `BufRead` as an event depending on your preference
+  event = "BufReadPost",   -- You can also use `VimEnter` or `BufRead` as an event depending on your preference
   config = function()
     -- Set up Copilot
     require("copilot").setup({
@@ -12,21 +12,20 @@ return {
         auto_trigger = true,
         hide_during_completion = false,
         keymap = {
-          accept = "<C-J>",  -- Customize the keymap to accept suggestions (this will use Ctrl+J by default)
-          next = "<M-]>",    -- Use Alt+Right for the next suggestion
-          prev = "<M-[>",    -- Use Alt+Left for the previous suggestion
+          accept = "<C-Q>", -- Customize the keymap to accept suggestions (this will use Ctrl+J by default)
+          next = "<M-]>",   -- Use Alt+Right for the next suggestion
+          prev = "<M-[>",   -- Use Alt+Left for the previous suggestion
         },
       },
       panel = {
-        enabled = false,  -- Option to disable the Copilot panel (set to true if you want the panel)
+        enabled = false, -- Option to disable the Copilot panel (set to true if you want the panel)
       },
       filetypes = {
-        markdown = true,  -- Enable Copilot for markdown files
-        help = true,      -- Enable Copilot for help files
+        markdown = true, -- Enable Copilot for markdown files
+        help = true,     -- Enable Copilot for help files
+        typescriptreact = true,
+        javascriptreact = true,
       },
     })
-
-    -- Optionally, set key mappings here
-    vim.api.nvim_set_keymap("i", "<C-J>", "copilot#Accept()", { silent = true, expr = true, noremap = true })
   end,
 }
