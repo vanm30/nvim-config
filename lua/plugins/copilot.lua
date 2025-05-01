19ffc418c470c8f250930f8a67b0f4491a2b4a17
vanm30 <vanikmatej99@gmail.com>
2025-05-01 18:46:48 +0000

    migrate

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,28 +1,14 @@
 return {
-  -- Enable Copilot
-  "zbirenbaum/copilot.lua",
-  cmd = "Copilot",         -- This ensures Copilot is loaded when the command is called
-  build = ":Copilot auth", -- Ensures authentication for Copilot
-  event = "BufReadPost",   -- You can also use `VimEnter` or `BufRead` as an event depending on your preference
+  "Exafunction/codeium.nvim",
+  cmd = "Codeium",         -- Lazy-load on command
+  build = ":Codeium Auth", -- Ensure user authenticates
+  event = "BufReadPost",   -- Load on buffer open
   config = function()
-    -- Set up Copilot
-    require("copilot").setup({
-      suggestion = {
-        enabled = true,
-        auto_trigger = true,
-        hide_during_completion = false,
-        keymap = {
-          accept = "<C-Q>", -- Customize the keymap to accept suggestions (this will use Ctrl+J by default)
-          next = "<M-]>",   -- Use Alt+Right for the next suggestion
-          prev = "<M-[>",   -- Use Alt+Left for the previous suggestion
-        },
-      },
-      panel = {
-        enabled = false, -- Option to disable the Copilot panel (set to true if you want the panel)
-      },
+    require("codeium").setup({
+      enable_tab_fallback = true, -- if true, Codeium fallback uses <Tab> if no suggestion
       filetypes = {
-        markdown = true, -- Enable Copilot for markdown files
-        help = true,     -- Enable Copilot for help files
+        markdown = true,
+        help = true,
         typescriptreact = true,
         javascriptreact = true,
         python = true,
@@ -31,5 +17,23 @@ return {
         json = true,
       },
     })
+
+    -- Custom keymaps for accepting suggestions
+    vim.keymap.set("i", "<C-Q>", function()
+      return vim.fn["codeium#Accept"]()
+    end, { expr = true, silent = true })
+
+    vim.keymap.set("i", "<M-]>", function()
+      return vim.fn
+    end, { expr = true, silent = true })
+
+    vim.keymap.set("i", "<M-[>", function()
+      return vim.fn["codeium#CycleCompletions"](-1)
+    end, { expr = true, silent = true })
+
+    vim.keymap.set("i", "<C-x>", function()
+      return vim.fn["codeium#Clear"]()
+    end, { expr = true, silent = true })
   end,
 }
+
